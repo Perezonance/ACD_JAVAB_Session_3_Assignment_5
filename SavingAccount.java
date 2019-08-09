db246e4053fc34a880eca2f37073124a9d2279c6
Perezonance <53836343+Perezonance@users.noreply.github.com>
2019-08-09 11:29:15 -0400

    Add files via upload

---
+++ SavingAccount.java
@@ -0,0 +1,15 @@
+package bankManagementSystem;
+
+public class SavingAccount extends Account{
+	private double rateOfInt;
+	
+	public SavingAccount(int no, String name, String bal){
+		super(no, bal, bal);
+	}
+	
+	@Override
+		public void display() {
+			// TODO Auto-generated method stub
+			super.display();
+		}	
+}
